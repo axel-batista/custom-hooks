c0f62fec112ebaf5456936b7c14f8deb09206003
Axel <axelbatistainfante@gmail.com>
2024-09-27 09:44:25 -0400

    Primer commit

---
+++ useTodos.js
@@ -0,0 +1,56 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+const initialState = [];
+
+const init = () => {
+	return JSON.parse(localStorage.getItem("todos")) || [];
+};
+
+export const useTodos = () => {
+	const [todos, dispatchTodo] = useReducer(todoReducer, initialState, init);
+
+	useEffect(() => {
+		localStorage.setItem("todos", JSON.stringify(todos));
+	}, [todos]);
+
+	const handleNewTodo = (todo) => {
+		const action = {
+			type: "[TODO] Add Todo",
+			payload: todo,
+		};
+
+		dispatchTodo(action);
+
+		// console.log({ todo });
+	};
+
+	const handleDeleteTodo = (id) => {
+		const action = {
+			type: "[TODO] Remove Todo",
+			payload: id,
+		};
+
+		dispatchTodo(action);
+	};
+
+	const handleToggleTodo = (id) => {
+		const action = {
+			type: "[TODO] Toggle Todo",
+			payload: id,
+		};
+		dispatchTodo(action);
+	};
+
+	const todosCount = todos.length;
+	const pendingTodosCount = todos.filter((todo) => !todo.done).length;
+
+	return {
+		todos,
+		handleNewTodo,
+		handleDeleteTodo,
+		handleToggleTodo,
+		todosCount,
+		pendingTodosCount,
+	};
+};
